72b92fb0a2b7bdad595cb8c8c1b20e92b7e7c2f8
Nikolay Novikov <AnourValar@gmail.com>
2022-12-22 14:20:17 +0300

    patch

--- src/Builders/Operations/InOperation.php
+++ src/Builders/Operations/InOperation.php
@@ -29,7 +29,7 @@ class InOperation implements OperationInterface
      */
     public function passes($value): bool
     {
-        return isset($value);
+        return isset($value) && $value !== [];
     }
 
     /**
