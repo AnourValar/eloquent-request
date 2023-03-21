19a210f247a1e6e8a4c5782a8ac5b68aab45590e
Nikolay Novikov <AnourValar@gmail.com>
2023-03-21 13:55:11 +0300

    patch

--- src/Adapters/CanonicalAdapter.php
+++ src/Adapters/CanonicalAdapter.php
@@ -10,6 +10,10 @@ class CanonicalAdapter implements AdapterInterface
      */
     public function canonize(array $request, array $profile, array $config): array
     {
+        if (isset($request['eloquent_request']) && is_string($request['eloquent_request'])) {
+            return json_decode($request['eloquent_request'], true) ?? [];
+        }
+
         return $request;
     }
 }
